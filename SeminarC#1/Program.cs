d2b32fe7c46db6e50d4457cd912a51fff8855d84
Konstantin <kostik-kostik-12@mail.ru>
2023-04-20 21:19:48 +0300

    add SeminarC#1

---
+++ SeminarC#1/Program.cs
@@ -0,0 +1,7 @@
+﻿//Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя). Например:4 -> 16   -3 -> 9   -7 -> 49
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+//Console.WriteLine("Квадрат нашего числа = " + number * number);
+int sqr = number * number;
+Console.WriteLine("Квадрат нашего числа = " + sqr);
